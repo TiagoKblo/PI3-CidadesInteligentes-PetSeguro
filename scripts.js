5e14afc76e7e80aa99b044defdde4fa12bf5f2ab
TiagoKblo <kblotiago@gmail.com>
2023-10-19 10:12:45 -0300

    criação cadastro usuario refeito rodapé

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
 
 // Initialize AOS
 AOS.init();
+
+// Função para verificar se a página rolou até o final
+function isPageAtBottom() {
+  return (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+}
+
+// Função para mostrar ou ocultar o rodapé com base na posição da página
+function toggleFooterVisibility() {
+  if (isPageAtBottom()) {
+      document.getElementById('myFooter').style.display = 'block';
+  } else {
+      document.getElementById('myFooter').style.display = 'none';
+  }
+}
+
+// Adicionar um ouvinte de evento de rolagem
+window.addEventListener('scroll', toggleFooterVisibility);
+
+// Chamar a função inicialmente para verificar o estado da página
+toggleFooterVisibility();
