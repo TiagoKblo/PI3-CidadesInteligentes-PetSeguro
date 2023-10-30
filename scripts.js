3f658317473213ff6f4f1d0d159052f61467c337
TiagoKblo <kblotiago@gmail.com>
2023-10-30 11:40:46 -0300

    correção do erro do javascript

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
 // Initialize AOS
 AOS.init();
 
-// Função para redirecionar para a página de cadastro pet
-document.addEventListener("DOMContentLoaded", function() {
+// Verifica se a página atual é "cadastro.html" antes de adicionar o evento
+if (window.location.pathname === '/cadastro.html') {
   // Obtém o elemento de formulário com o ID "cadastro-form"
   const form = document.getElementById("cadastro-form");
 
-  // Adiciona um evento de submissão do formulário
-  form.addEventListener("submit", function(event) {
-    // Redireciona para a página "cadastropet.html" quando o formulário é enviado
-    window.location.href = "cadastropet.html";
-    
-    // Impede que o formulário seja submetido normalmente (evita recarregar a página)
-    event.preventDefault();
-  });
-});
+  // Verifica se o elemento foi encontrado antes de adicionar o ouvinte de evento
+  if (form) {
+    // Adiciona um evento de submissão do formulário
+    form.addEventListener("submit", function(event) {
+      // Redireciona para a página "cadastropet.html" quando o formulário é enviado
+      window.location.href = "cadastropet.html";
+
+      // Impede que o formulário seja submetido normalmente (evita recarregar a página)
+      event.preventDefault();
+    });
+  }
+}
