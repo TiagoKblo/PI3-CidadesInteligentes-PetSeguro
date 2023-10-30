e137feb9dbbd227c576b29843351ba3e53089831
TiagoKblo <kblotiago@gmail.com>
2023-10-30 13:58:09 -0300

    deixando a navbar fixa e alterando page informacao

--- graficos.js
+++ graficos.js
@@ -1,21 +1,28 @@
+// Inicializa os gráficos
 function inicializarGraficos() {
     // Use a função fetch para carregar o arquivo JSON
     fetch('dadoscadastropet.json')
         .then(response => response.json())
         .then(data => {
             // Agora, 'data' contém os dados do arquivo JSON
-            criarGraficoTiposSanguineos(data.tiposSanguineos);
-            criarGraficoEspecies(data.especies);
-            criarGraficoIdades(data.idades);
+            const tiposSanguineos = data.map(pet => pet.tipo_sanguineo);
+            const especies = data.map(pet => pet.especie);
+            const idades = data.map(pet => calcularIdade(pet.data_nascimento, pet.data_obito));
+            const doencas = data.map(pet => pet.doencas_conhecidas.length > 0 ? "Com Doença" : "Sem Doença");
+            const vacinas = data.reduce((acc, pet) => {
+                return acc.concat(pet.vacinas.map(vacina => `${vacina.tipo_vacina}-${vacina.fabricante_vacina}`));
+            }, []);
+
+            criarGraficoEspecies(especies);
+            criarGraficoIdades(idades);
+            criarGraficoDadosGerais(data);
+            criarGraficoDoencas(data);
+            criarGraficoVacinas(data);
         })
         .catch(error => {
             console.error('Erro ao carregar o arquivo JSON:', error);
         });
 }
-
-
-// Função para criar o gráfico de barras (Tipos Sanguíneos)
-
 
 // Função para criar o gráfico de pizza (Espécies)
 function criarGraficoEspecies(especies) {
@@ -44,93 +51,76 @@ function criarGraficoEspecies(especies) {
     });
 }
 
-// Função para criar o gráfico de linha (Idades)
-function criarGraficoIdades(idades) {
-    const ctx = document.getElementById("line-chart-idades").getContext("2d");
-
-    new Chart(ctx, {
-        type: "line",
-        data: {
-            labels: idades.map(String),
-            datasets: [{
-                label: "Idades de Pets",
-                data: idades,
-                borderColor: "rgba(75, 192, 192, 1)",
-                borderWidth: 2,
-                fill: false,
-            }],
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-}
-// Função para criar o gráfico de barras (Dados Gerais: Espécie, Sexo, Tipo Sanguíneo)
+// Função para criar o gráfico de barras (Dados Gerais: Espécie, Sexo)
 function criarGraficoDadosGerais(pets) {
     const ctx = document.getElementById("bar-chart-dados-gerais").getContext("2d");
 
-    const dadosGerais = pets.map(pet => {
-        return {
-            especie: pet.especie,
-            sexo: pet.sexo,
-            tipo_sanguineo: pet.tipo_sanguineo,
-        };
-    });
+    const especies = [...new Set(pets.map(pet => pet.especie))];
+    const sexos = [...new Set(pets.map(pet => pet.sexo))];
+    const datasets = [];
 
-    const contagemDadosGerais = dadosGerais.reduce((acc, dados) => {
-        const chave = `${dados.especie}-${dados.sexo}-${dados.tipo_sanguineo}`;
-        acc[chave] = (acc[chave] || 0) + 1;
-        return acc;
-    }, {});
+    especies.forEach(especie => {
+        const data = sexos.map(sexo => {
+            const count = pets.filter(pet => pet.especie === especie && pet.sexo === sexo).length;
+            return count;
+        });
+
+        datasets.push({
+            label: especie,
+            data: data,
+            backgroundColor: getBackgroundColor(especie),
+        });
+    });
 
     new Chart(ctx, {
         type: "bar",
         data: {
-            labels: Object.keys(contagemDadosGerais),
-            datasets: [{
-                label: "Contagem de Dados Gerais",
-                data: Object.values(contagemDadosGerais),
-                backgroundColor: [
-                    "rgba(75, 192, 192, 0.2)",
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(255, 206, 86, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                    "rgba(153, 102, 255, 0.2)",
-                ],
-                borderColor: [
-                    "rgba(75, 192, 192, 1)",
-                    "rgba(255, 99, 132, 1)",
-                    "rgba(255, 206, 86, 1)",
-                    "rgba(54, 162, 235, 1)",
-                    "rgba(153, 102, 255, 1)",
-                ],
-                borderWidth: 1,
-            }],
+            labels: sexos,
+            datasets: datasets,
         },
         options: {
             scales: {
+                x: {
+                    stacked: false, // Barras não empiladas
+                },
                 y: {
-                    beginAtZero: true
-                }
-            }
-        }
+                    beginAtZero: true,
+                },
+            },
+        },
     });
 }
 
-// Função para criar o gráfico de barras (Se possui alguma doença ou não)
+function getBackgroundColor(especie) {
+    // Define cores fixas com base nas espécies
+    if (especie === 'Cachorro') {
+        return ['rgba(54, 162, 235, 0.5)']; // Azul para Cachorros
+    } else if (especie === 'Gato') {
+        return ['rgba(255, 99, 132, 0.5)']; // Vermelho para Gatos
+    } else {
+        return ['rgba(125, 125, 125, 0.5)', 'rgba(125, 125, 125, 0.5)']; // Cor padrão
+    }
+}
+
+
+
+
+
 function criarGraficoDoencas(pets) {
     const ctx = document.getElementById("bar-chart-doenca").getContext("2d");
 
-    const doencas = pets.map(pet => pet.doencas_conhecidas.length > 0 ? "Com Doença" : "Sem Doença");
+    const contagemDoencas = {
+        "Com Doença": 0,
+        "Sem Doença": 0
+    };
 
-    const contagemDoencas = doencas.reduce((acc, doenca) => {
-        acc[doenca] = (acc[doenca] || 0) + 1;
-        return acc;
-    }, {});
+    pets.forEach(pet => {
+        if (pet.doencas_conhecidas.length > 0) {
+            contagemDoencas["Com Doença"]++;
+        } else {
+            contagemDoencas["Sem Doença"]++;
+        }
+    });
 
     new Chart(ctx, {
         type: "bar",
@@ -159,6 +149,7 @@ function criarGraficoDoencas(pets) {
         }
     });
 }
+
 
 // Função para criar o gráfico de barras (Vacinas)
 function criarGraficoVacinas(pets) {
@@ -205,28 +196,34 @@ function criarGraficoVacinas(pets) {
     });
 }
 
-// Inicializa os gráficos
-function inicializarGraficos() {
-    // Use a função fetch para carregar o arquivo JSON
-    fetch('dadoscadastropet.json')
-        .then(response => response.json())
-        .then(data => {
-            // Agora, 'data' contém os dados do arquivo JSON
-            const tiposSanguineos = data.map(pet => pet.tipo_sanguineo);
-            const especies = data.map(pet => pet.especie);
-            const idades = data.map(pet => calcularIdade(pet.data_nascimento, pet.data_obito));
 
-            
-            criarGraficoEspecies(especies);
-            criarGraficoIdades(idades);
-            criarGraficoDadosGerais(data);
-            criarGraficoDoencas(data);
-            criarGraficoVacinas(data);
-        })
-        .catch(error => {
-            console.error('Erro ao carregar o arquivo JSON:', error);
-        });
+// Função para criar o gráfico de linha (Idades)
+function criarGraficoIdades(idades) {
+    const ctx = document.getElementById("line-chart-idades").getContext("2d");
+
+    new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: idades.map(String),
+            datasets: [{
+                label: "Idades de Pets",
+                data: idades,
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderWidth: 2,
+                fill: false,
+            }],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 }
+
+
 
 // Função para calcular a idade com base na data de nascimento e óbito
 function calcularIdade(dataNascimento, dataObito) {
