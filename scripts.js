424121e962b5d32ffad5b593d194a67c55bea620
Judoloko <judoloko.85@gmail.com>
2023-11-15 00:05:12 -0300

    Adicionando Api de cep

--- scripts.js
+++ scripts.js
@@ -73,3 +73,49 @@ if (document.location.pathname.endsWith("cadastropet.html")) {
     }
   }
 }
+
+// API DE CEP 
+
+const cep = document.querySelector('#cep');
+const estado = document.querySelector('#estado');
+const cidade = document.querySelector('#cidade');
+const bairro = document.querySelector('#bairro');
+const rua = document.querySelector('#rua');
+const message = document.querySelector('#message');
+
+cep.addEventListener('focusout', async () => {
+  
+  try {
+    const onlyNumbers = /^[0-9]+$/;
+    const cepValid = /^[0-9]+$/;
+  
+    if(!onlyNumbers.test(cep.value) || !cepValid.test(cep.value)){
+      throw {cep_error: 'Cep Inválido'};
+    }
+
+    const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
+
+    if(!response.ok) {
+      throw await response.json();
+    }
+
+    const responseCep = await response.json();
+
+    estado.value = responseCep.uf; 
+    cidade.value = responseCep.localidade;
+    bairro.value = responseCep.bairro;
+    rua.value = responseCep.logradouro;
+
+
+
+  } catch (error) {
+    if(error?.cep_error){
+      message.textContent = error.error_cep;
+      setTimeout(() => {
+        message.textContent = "";
+      }, 5000);
+    }
+    console.log(error);
+  }
+
+})
