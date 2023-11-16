16b3afc0240b12bf1f5fcbc3fa48d84cdc49ce54
TiagoKblo <kblotiago@gmail.com>
2023-11-16 09:54:27 -0300

    alteração pagina informações inclusão mapa

---
+++ map.js
@@ -0,0 +1,7 @@
+function initMap() {
+  const myLatLng = { lat: -22.4334, lng: -46.8222 }; // Coordenadas da cidade de Itapira, SP, Brasil
+  const map = new google.maps.Map(document.getElementById('map'), {
+      center: myLatLng,
+      zoom: 8
+  });
+}
