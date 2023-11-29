bd0bbbf827ce631221d57510ad645c8a54d66f79
Tiago Henrique de Moraes <111241469+TiagoKblo@users.noreply.github.com>
2023-11-29 14:09:44 +0000

    adicionando animais perdidos ao mapa

--- app/lost-pets.js
+++ app/lost-pets.js
@@ -1,28 +1,38 @@
-function addLostPet() {
-  const petName = document.getElementById('pet-name').value;
-  const petDescription = document.getElementById('pet-description').value;
+// Exemplo de dados de animais perdidos
+const lostPetsData = [
+    {
+      name: 'Cachorro Perdido',
+      type: 'Cachorro',
+      color: 'Marrom',
+      coordinates: { lat: -22.439, lng: -46.820 }
+    },
+    // Adicione mais animais conforme necessário
+  ];
 
-  if (movableMarker) {
-      const petLatLng = movableMarker.getPosition();
+  // Função para carregar marcadores de animais perdidos no mapa
+  function loadLostPetsMarkers(map) {
+    // Ícone do animal com tamanho menor (por exemplo, 10x10)
+    const animalIconSize = new google.maps.Size(25, 25);
 
-      // Adicione um marcador para a localização do animal perdido
-      new google.maps.Marker({
-          position: petLatLng,
-          map: map,
-          title: petName
+    // Crie um objeto de ícone para o animal com a propriedade scaledSize definida
+    const animalIcon = {
+      url: '/imagens/icone-pata.png',
+      scaledSize: animalIconSize
+    };
+
+    lostPetsData.forEach(function (animal) {
+      const marker = new google.maps.Marker({
+        position: animal.coordinates,
+        map: map,
+        title: animal.name,
+        icon: animalIcon // Usando o ícone personalizado com o tamanho ajustado
       });
 
-      // Aqui você pode armazenar as informações do animal perdido no seu banco de dados ou em outra estrutura de dados
-      // Exemplo: petDatabase.addLostPet(petName, petDescription, petLatLng);
-
-      // Limpe o formulário após o cadastro
-      document.getElementById('pet-name').value = '';
-      document.getElementById('pet-description').value = '';
-
-      // Remova o marcador móvel
-      movableMarker.setMap(null);
-      movableMarker = null;
-  } else {
-      alert('Escolha a localização do animal no mapa.');
+      marker.addListener('click', function () {
+        showAnimalInfo(animal);
+      });
+    });
   }
-}
+
+  // Chame a função para carregar os marcadores de animais perdidos
+  loadLostPetsMarkers(map);
--- app/map.js
+++ app/map.js
@@ -1,4 +1,4 @@
-let movableMarker;
+let animalInfoDiv;
 
 function initMap() {
   // Use a Geocoding API para obter as coordenadas para "Itapira, SP, Brasil"
@@ -6,25 +6,47 @@ function initMap() {
   const location = "Itapira, SP, Brasil";
 
   geocoder.geocode({ 'address': location }, function (results, status) {
-      if (status === 'OK') {
-          const myLatLng = results[0].geometry.location;
-          const map = new google.maps.Map(document.getElementById('map'), {
-              center: myLatLng,
-              zoom: 11
-          });
+    if (status === 'OK') {
+      const myLatLng = results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 11
+      });
 
-          // Adicione um marcador para a localização
-          new google.maps.Marker({
-              position: myLatLng,
-              map: map,
-              title: location
-          });
+      // Ícone do animal com tamanho menor (por exemplo, 10x10)
+      const animalIconSize = new google.maps.Size(25, 25);
 
-          // Carregue e exiba os polígonos do GeoJSON
-          loadGeoJson(map);
-      } else {
-          console.error('Erro ao obter coordenadas:', status);
-      }
+      // Crie um objeto de ícone para o animal com a propriedade scaledSize definida
+      const animalIcon = {
+        url: '/imagens/icone-pata.png',
+        scaledSize: animalIconSize
+      };
+
+      // Ícone da cidade
+      const cityIconSize = new google.maps.Size(20, 20);
+
+      // Crie um objeto de ícone para a cidade com a propriedade scaledSize definida
+      const cityIcon = {
+        url: '/imagens/icone-cidade.png',
+        scaledSize: cityIconSize
+      };
+
+      // Adicione um marcador para a localização da cidade com o ícone personalizado
+      new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: location,
+        icon: cityIcon
+      });
+
+      // Carregue e exiba os polígonos do GeoJSON
+      loadGeoJson(map);
+
+      // Adicione marcadores de animais perdidos
+      loadLostPetsMarkers(map);
+    } else {
+      console.error('Erro ao obter coordenadas:', status);
+    }
   });
 }
 
@@ -34,31 +56,43 @@ function loadGeoJson(map) {
 
   // Defina estilos para os polígonos
   map.data.setStyle({
-      fillColor: 'green',
-      strokeWeight: 1
+    fillColor: 'green',
+    strokeWeight: 1
   });
 }
-map.addListener('click', function (event) {
-    if (!movableMarker) {
-        movableMarker = new google.maps.Marker({
-            position: event.latLng,
-            map: map,
-            draggable: true, // Torna o marcador móvel
-            title: 'Escolher Localização'
-        });
 
-        // Adicione um ouvinte para atualizar as coordenadas quando o marcador for movido
-        movableMarker.addListener('dragend', function () {
-            updateLocation(movableMarker.getPosition());
-        });
-    } else {
-        movableMarker.setPosition(event.latLng);
-        updateLocation(movableMarker.getPosition());
-    }
-});
+// Função para carregar marcadores de animais perdidos no mapa
+function loadLostPetsMarkers(map) {
+  // Exemplo de dados de animais perdidos
+  const lostPetsData = [
+    {
+      name: 'Cachorro Perdido',
+      type: 'Cachorro',
+      color: 'Marrom',
+      coordinates: { lat: -22.439, lng: -46.820 }
+    },
+    // Adicione mais animais conforme necessário
+  ];
 
+  lostPetsData.forEach(function (animal) {
+    const marker = new google.maps.Marker({
+      position: animal.coordinates,
+      map: map,
+      title: animal.name,
+      icon: animalIcon // Usando o ícone personalizado com o tamanho ajustado
+    });
 
-function updateLocation(latLng) {
-// Atualize a localização no seu formulário ou onde quer que você esteja armazenando as coordenadas
-document.getElementById('location').value = latLng.lat() + ', ' + latLng.lng();
+    // Adicione um evento de clique ao marcador para exibir informações do animal
+    marker.addListener('click', function () {
+      showAnimalInfo(animal);
+    });
+  });
+}
+
+// Função para exibir informações do animal na div abaixo do mapa
+function showAnimalInfo(info) {
+  document.getElementById('animal-name').innerText = info.name;
+  document.getElementById('animal-type').innerText = info.type;
+  document.getElementById('animal-color').innerText = info.color;
+  // Adicione mais campos conforme necessário
 }
