05a83a9bdefb169d5b19e54a9b98e63c8a072255
TiagoKblo <kblotiago@gmail.com>
2023-12-01 17:41:25 -0300

    quadrante e marcadores funcionando

--- app/mapBairro.js
+++ app/mapBairro.js
@@ -31,12 +31,13 @@ function gerarQuadradoInferiorEsquerdo(lat, lng, largura, altura) {
 
 function gerarQuadradoInferiorDireito(lat, lng, largura, altura) {
     const superiorEsquerdo = { lat: lat - altura, lng: lng };
-    const inferiorEsquerdo = { lat: lat - altura, lng: lng - largura };
+    const inferiorEsquerdo = { lat: lat, lng: lng };
     const superiorDireito = { lat: lat - altura, lng: lng + largura };
-    const inferiorDireito = { lat: lat - altura, lng: lng };
+    const inferiorDireito = { lat: lat, lng: lng + largura };
 
     return { superiorEsquerdo, inferiorEsquerdo, superiorDireito, inferiorDireito };
 }
+
 
 function initMap() {
     const geocoder = new google.maps.Geocoder();
@@ -47,7 +48,7 @@ function initMap() {
             const myLatLng = results[0].geometry.location;
             const map = new google.maps.Map(document.getElementById('map'), {
                 center: myLatLng,
-                zoom: 11
+                zoom: 12
             });
 
             const cityIconSize = new google.maps.Size(20, 20);
@@ -70,10 +71,17 @@ function initMap() {
         }
     });
 }
-
+function adicionarMarcador(map, position, label) {
+    new google.maps.Marker({
+        position: position,
+        map: map,
+        label: label,
+        draggable: false,
+    });
+}
 function adicionarQuadrantes(map, coordenadaCentral) {
-    const larguraQuadrante = 0.02;
-    const alturaQuadrante = 0.02;
+    const larguraQuadrante = 0.05;
+    const alturaQuadrante = 0.05;
 
     const quadradoSuperiorEsquerdo = gerarQuadradoSuperiorEsquerdo(coordenadaCentral.lat(), coordenadaCentral.lng(), larguraQuadrante, alturaQuadrante);
     const quadradoSuperiorDireito = gerarQuadradoSuperiorDireito(coordenadaCentral.lat(), coordenadaCentral.lng(), larguraQuadrante, alturaQuadrante);
@@ -89,7 +97,30 @@ function adicionarQuadrantes(map, coordenadaCentral) {
     adicionarPoligono(map, pontoSE1, pontoIE1, pontoID1, pontoSD1, '#FF0000');
     adicionarPoligono(map, pontoIE2, pontoSE2, pontoSD2, pontoID2, '#00FF00');
     adicionarPoligono(map, pontoSD3, pontoID3, pontoIE3, pontoSE3, '#0000FF');
-    adicionarPoligono(map, pontoID4, pontoSD4, pontoSE4, pontoIE4, '#FFFF00');
+    adicionarPoligono(map, pontoIE4, pontoSE4, pontoSD4, pontoID4, '#FFFF00');
+
+    const pontoMedioNE = {
+        lat: (pontoSE1.lat + pontoSD1.lat + pontoIE1.lat + pontoID1.lat) / 4,
+        lng: (pontoSE1.lng + pontoSD1.lng + pontoIE1.lng + pontoID1.lng) / 4
+    };
+    const pontoMedioNW = {
+        lat: (pontoSE2.lat + pontoSD2.lat + pontoIE2.lat + pontoID2.lat) / 4,
+        lng: (pontoSE2.lng + pontoSD2.lng + pontoIE2.lng + pontoID2.lng) / 4
+    };
+    const pontoMedioSW = {
+        lat: (pontoSE3.lat + pontoSD3.lat + pontoIE3.lat + pontoID3.lat) / 4,
+        lng: (pontoSE3.lng + pontoSD3.lng + pontoIE3.lng + pontoID3.lng) / 4
+    };
+    const pontoMedioSE = {
+        lat: (pontoSE4.lat + pontoSD4.lat + pontoIE4.lat + pontoID4.lat) / 4,
+        lng: (pontoSE4.lng + pontoSD4.lng + pontoIE4.lng + pontoID4.lng) / 4
+    };
+
+    // Adicionar marcadores
+    adicionarMarcador(map, pontoMedioNE, "NE");
+    adicionarMarcador(map, pontoMedioNW, "NW");
+    adicionarMarcador(map, pontoMedioSW, "SW");
+    adicionarMarcador(map, pontoMedioSE, "SE");
 }
 
 
