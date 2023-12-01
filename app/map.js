af0ee64f2781b913fa8efdd5c5b5d654d3d47c03
TiagoKblo <kblotiago@gmail.com>
2023-12-01 18:45:27 -0300

    puxando animais do json e criando marcadores

--- app/map.js
+++ app/map.js
@@ -1,6 +1,20 @@
 let animalInfoDiv;
 let animalIcon;
+// Função de geocodificação para obter a latitude e longitude com base no endereço
+function geocodeAddress(address, callback) {
+    const geocoder = new google.maps.Geocoder();
 
+    // Note que não estamos passando a chave da API aqui, pois já está incluída no script do Google Maps no HTML
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status === 'OK') {
+            const location = results[0].geometry.location;
+            const latLng = { lat: location.lat(), lng: location.lng() };
+            callback(latLng);
+        } else {
+            console.error('Erro ao geocodificar o endereço:', status);
+        }
+    });
+}
 function initMap() {
     // Use a Geocoding API para obter as coordenadas para "Itapira, SP, Brasil"
     const geocoder = new google.maps.Geocoder();
@@ -52,55 +66,45 @@ function loadGeoJson(map) {
     });
 }
 
-// Função para carregar marcadores de animais perdidos no mapa
+
+
+// Função para carregar marcadores de animais perdidos no mapa a partir de um arquivo JSON
 function loadLostPetsMarkers(map) {
-    const lostPetsData = [
-        {
-            name: 'Cachorro Perdido',
-            type: 'Cachorro',
-            color: 'Marrom',
-            coordinates: { lat: -22.439, lng: -46.830 }
-        },
-        {
-            name: 'TOTO',
-            type: 'Cachorro',
-            color: 'Amarelo',
-            coordinates: { lat: -22.440, lng: -46.815 }
-        },
-        {
-            name: 'Garfield',
-            type: 'Gato',
-            color: 'Laranja',
-            coordinates: { lat: -22.410, lng: -46.840 }
-        }
-    ];
+    const jsonFilePath = 'dados_animais.json';
 
-    // Ícone do animal com tamanho menor (por exemplo, 10x10)
-    const animalIconSize = new google.maps.Size(25, 25);
+    fetch(jsonFilePath)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao carregar dados do arquivo JSON');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const lostPetsData = data.filter(animal => animal['animal-perdido'] === 'sim');
 
-    // Crie um objeto de ícone para o animal com a propriedade scaledSize definida
-    animalIcon = {
-        url: '/imagens/icone-pata.png',
-        scaledSize: animalIconSize
-    };
+            const animalIconSize = new google.maps.Size(25, 25);
+            const animalIcon = {
+                url: '/imagens/icone-pata.png',
+                scaledSize: animalIconSize
+            };
 
-    lostPetsData.forEach(function (animal) {
-        const marker = new google.maps.Marker({
-            position: animal.coordinates,
-            map: map,
-            title: animal.name,
-            icon: animalIcon
-        });
+            lostPetsData.forEach(function (animal) {
+                // Certifique-se de que as chaves e os valores existem no seu JSON
+                const address = `${animal.rua}, ${animal.numero}, ${animal.bairro}, ${animal.cidade}, ${animal.estado}, ${animal.cep}`;
 
-        marker.addListener('click', function () {
-            showAnimalInfo(animal);
-        });
-    });
-}
+                geocodeAddress(address, function (latLng) {
+                    const marker = new google.maps.Marker({
+                        position: latLng,
+                        map: map,
+                        title: animal.nome,
+                        icon: animalIcon
+                    });
 
-function showAnimalInfo(info) {
-    document.getElementById('animal-name').innerText = info.name;
-    document.getElementById('animal-type').innerText = info.type;
-    document.getElementById('animal-color').innerText = info.color;
-    // Adicione mais campos conforme necessário
+                    marker.addListener('click', function () {
+                        showAnimalInfo(animal);
+                    });
+                });
+            });
+        })
+        .catch(error => console.error(error.message));
 }
